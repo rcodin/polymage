4d3d7e9bca4b76ba1013541d91305dafb6a62d2b
vinay_vasista <vinay.vasista@csa.iisc.ernet.in>
2016-06-22 21:18:33 +0530

    add PIL, numba versions of unsharp_mask

--- sandbox/video_demo/unsharp_mask/unsharp_process.py
+++ sandbox/video_demo/unsharp_mask/unsharp_process.py
@@ -6,10 +6,40 @@ import sys
 sys.path.insert(0, "../")
 from structs import *
 
+from PIL import Image, ImageFilter
+from unsharp_numba_version import unsharp_numba
+
+#from numpy import jit
+
 # unsharp mask parameters
 thresh = 0.001
 weight = 3
 
+# PIL version
+#@jit("uint8[::](uint8[::], int64)", cache = True, nogil = True)
+def unsharp_pil(frame, lib_func):
+    im = Image.fromarray(frame)
+    m = im.filter(ImageFilter.UnsharpMask(radius = 2, \
+            percent = 130, threshold = 1))
+    res = np.array(m)
+
+    return res
+
+# OpenCV version
+#@jit("float32[::](float32[::], int64)", cache = True, nogil = True)
+def unsharp_cv(frame, lib_func):
+    res = frame
+    kernelx = np.array([1, 4, 6, 4, 1], np.float32) / 16
+    kernely = np.array([[1], [4], [6], [4], [1]], np.float32) / 16
+    blury = sepFilter2D(frame, -1, kernelx, kernely)
+    sharpen = addWeighted(frame, (1 + weight), blury, (-weight), 0)
+    th, choose = threshold(absdiff(frame, blury), thresh, 1, THRESH_BINARY)
+    choose = choose.astype(bool)
+    np.copyto(res, sharpen, 'same_kind', choose)
+
+    return res
+
+'''
 def unsharp_cv(frame, lib_func):
     kernel = np.array([[1,4,6,4,1]], np.float32) / 16.0
     blurx = filter2D(frame, -1, kernel)
@@ -22,6 +52,7 @@ def unsharp_cv(frame, lib_func):
     np.copyto(res, sharpen, 'same_kind', mask)
 
     return res
+'''
 
 def unsharp_polymage(frame, lib_func):
     rows = frame.shape[0]
@@ -37,7 +68,8 @@ def unsharp_polymage(frame, lib_func):
 
 def add_unsharp_app(app_id):
     # 1. modes
-    modes = [ModeType.CV2, ModeType.P_NAIVE, ModeType.P_OPT]
+    modes = [ModeType.CV2, ModeType.P_NAIVE, ModeType.P_OPT, \
+        ModeType.PIL, ModeType.NUMBA]
     # 2. modes that need shared library
     lib_modes = [ModeType.P_NAIVE, ModeType.P_OPT]
 
@@ -46,6 +78,8 @@ def add_unsharp_app(app_id):
     app_func_map[ModeType.CV2] = unsharp_cv
     app_func_map[ModeType.P_NAIVE] = unsharp_polymage
     app_func_map[ModeType.P_OPT] = unsharp_polymage
+    app_func_map[ModeType.PIL] = unsharp_pil
+    app_func_map[ModeType.NUMBA] = unsharp_numba
 
     # 4. create an App object
     app_dir = os.path.dirname(os.path.realpath(__file__))
