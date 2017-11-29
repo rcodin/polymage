12db612199f4c9fae2d1a289ec4e12203d666064
Ronit Halder <ronit.crj@gmail.com>
2017-11-29 12:44:14 +0530

    Integration working

--- sandbox/video_demo/darknet/darknet_polymage.cpp
+++ sandbox/video_demo/darknet/darknet_polymage.cpp
@@ -4,80 +4,77 @@
 #include <cmath>
 #include <string.h>
 #include "../simple_pool_allocator.h"
+#include "../darknet.h"
+
 #define isl_min(x,y) ((x) < (y) ? (x) : (y))
 #define isl_max(x,y) ((x) > (y) ? (x) : (y))
 #define isl_floord(n,d) (((n)<0) ? -((-(n)+(d)-1)/(d)) : (n)/(d))
-extern "C" void  pipeline_opt(int  C, int  R, void * input_void_arg, void * filtered_void_arg)
+extern "C" void  pipeline_opt(int  C, int  R, network *net, char* datacfg)
 {
-    char *datacfg = "files/coco.data";
-    char *cfgfile = "files/yolo.cfg";
-    char *weightfile = "files/yolo.weights";
-    char *filename = "files/";
-    float thresh = .24;
-    float hier_thresh = .5;
-    char *outfile = NULL;
-    int fullscreen = 0;
+   // list *options = read_data_cfg(datacfg);
+   //  char *name_list = option_find_str(options, "names", "data/names.list");
+   //  char **names = get_labels(name_list);
+
+   //  char *filename = "../messigray.png";
+   //  float thresh = .5;
+   //  float hier_thresh = .5;
+   //  char *outfile = "try.png"; 
+   //  int fullscreen = 0;
 
 
-
-    list *options = read_data_cfg(datacfg);
-    char *name_list = option_find_str(options, "names", "data/names.list");
-    char **names = get_labels(name_list);
-
-    image **alphabet = load_alphabet();
-    network *net = load_network(cfgfile, weightfile, 0);
-    set_batch_network(net, 1);
-    srand(2222222);
-    double time;
-    char buff[256];
-    char *input = buff;
-    int j;
-    float nms=.3;
-        
-    if(filename){
-        strncpy(input, filename, 256);
-    } else {
-        printf("Enter Image Path: ");
-        fflush(stdout);
-        input = fgets(input, 256, stdin);
-        if(!input) return;
-        strtok(input, "\n");
-    }
-
-    image im;
+   //  image **alphabet = load_alphabet();
+   //  // network *net = load_network(cfgfile, weightfile, 0);
+   //  set_batch_network(net, 1);
+   //  srand(2222222);
+   //  double time;
+   //  char buff[256];
+   //  char *input = buff;
+   //  int j;
+   //  float nms=.3;
     
-    im.w = C;
-    im.h=
-    image sized = letterbox_image(im, net->w, net->h);
-    //image sized = resize_image(im, net->w, net->h);
-    //image sized2 = resize_max(im, net->w);
-    //image sized = crop_image(sized2, -((net->w - sized2.w)/2), -((net->h - sized2.h)/2), net->w, net->h);
-    //resize_network(net, sized.w, sized.h);
-    layer l = net->layers[net->n-1];
+   //      if(filename){
+   //          strncpy(input, filename, 256);
+   //      } else {
+   //          printf("Enter Image Path: ");
+   //          fflush(stdout);
+   //          input = fgets(input, 256, stdin);
+   //          if(!input) return;
+   //          strtok(input, "\n");
+   //      }
+   //      image im = load_image_color(input,0,0);
+   //      image sized = letterbox_image(im, net->w, net->h);
+   //      //image sized = resize_image(im, net->w, net->h);
+   //      //image sized2 = resize_max(im, net->w);
+   //      //image sized = crop_image(sized2, -((net->w - sized2.w)/2), -((net->h - sized2.h)/2), net->w, net->h);
+   //      //resize_network(net, sized.w, sized.h);
+   //      layer l = net->layers[net->n-1];
 
-    box *boxes = calloc(l.w*l.h*l.n, sizeof(box));
-    float **probs = calloc(l.w*l.h*l.n, sizeof(float *));
-    for(j = 0; j < l.w*l.h*l.n; ++j) probs[j] = calloc(l.classes + 1, sizeof(float *));
-    float **masks = 0;
-    if (l.coords > 4){
-        masks = calloc(l.w*l.h*l.n, sizeof(float*));
-        for(j = 0; j < l.w*l.h*l.n; ++j) masks[j] = calloc(l.coords-4, sizeof(float *));
-    }
+   //      box *boxes = (box*)calloc(l.w*l.h*l.n, sizeof(box));
+   //      float **probs = (float**)calloc(l.w*l.h*l.n, sizeof(float *));
+   //      for(j = 0; j < l.w*l.h*l.n; ++j) probs[j] = (float*)calloc(l.classes + 1, sizeof(float *));
+   //      float **masks = 0;
+   //      if (l.coords > 4){
+   //          masks = (float**)calloc(l.w*l.h*l.n, sizeof(float*));
+   //          for(j = 0; j < l.w*l.h*l.n; ++j) masks[j] = (float*)calloc(l.coords-4, sizeof(float *));
+   //      }
 
-    float *X = sized.data;
-    time=what_time_is_it_now();
-    network_predict(net, X);
-    printf("%s: Predicted in %f seconds.\n", input, what_time_is_it_now()-time);
-    get_region_boxes(l, im.w, im.h, net->w, net->h, thresh, probs, boxes, masks, 0, 0, hier_thresh, 1);
-    //if (nms) do_nms_obj(boxes, probs, l.w*l.h*l.n, l.classes, nms);
-    if (nms) do_nms_sort(boxes, probs, l.w*l.h*l.n, l.classes, nms);
-    draw_detections(im, l.w*l.h*l.n, thresh, boxes, probs, masks, names, alphabet, l.classes);
+   //      float *X = sized.data;
+   //      time=what_time_is_it_now();
+   //      network_predict(net, X);
+   //      printf("%s: Predicted in %f seconds.\n", input, what_time_is_it_now()-time);
+   //      get_region_boxes(l, im.w, im.h, net->w, net->h, thresh, probs, boxes, masks, 0, 0, hier_thresh, 1);
+   //      //if (nms) do_nms_obj(boxes, probs, l.w*l.h*l.n, l.classes, nms);
+   //      if (nms) do_nms_sort(boxes, probs, l.w*l.h*l.n, l.classes, nms);
+   //      draw_detections(im, l.w*l.h*l.n, thresh, boxes, probs, masks, names, alphabet, l.classes);
+   //      if(outfile){
+   //          save_image(im, outfile);
+   //      }
+        
 
-
-    free_image(im);
-    free_image(sized);
-    free(boxes);
-    free_ptrs((void **)probs, l.w*l.h*l.n);
+   //      free_image(im);
+   //      free_image(sized);
+   //      free(boxes);
+   //      free_ptrs((void **)probs, l.w*l.h*l.n);
 }
 
 
--- sandbox/video_demo/darknet/darknet_process.py
+++ sandbox/video_demo/darknet/darknet_process.py
@@ -6,18 +6,169 @@ import sys
 sys.path.insert(0, "../")
 from structs import *
 
+from ctypes import *
+import math
+import random
+
+def sample(probs):
+    s = sum(probs)
+    probs = [a/s for a in probs]
+    r = random.uniform(0, 1)
+    for i in range(len(probs)):
+        r = r - probs[i]
+        if r <= 0:
+            return i
+    return len(probs)-1
+
+def c_array(ctype, values):
+    arr = (ctype*len(values))()
+    arr[:] = values
+    return arr
+
+class BOX(Structure):
+    _fields_ = [("x", c_float),
+                ("y", c_float),
+                ("w", c_float),
+                ("h", c_float)]
+
+class IMAGE(Structure):
+    _fields_ = [("w", c_int),
+                ("h", c_int),
+                ("c", c_int),
+                ("data", POINTER(c_float))]
+
+class METADATA(Structure):
+    _fields_ = [("classes", c_int),
+                ("names", POINTER(c_char_p))]
+
+    
+
+#lib = CDLL("/home/pjreddie/documents/darknet/libdarknet.so", RTLD_GLOBAL)
+lib = CDLL("darknet/files/libdarknet.so", RTLD_GLOBAL)
+lib.network_width.argtypes = [c_void_p]
+lib.network_width.restype = c_int
+lib.network_height.argtypes = [c_void_p]
+lib.network_height.restype = c_int
+
+predict = lib.network_predict
+predict.argtypes = [c_void_p, POINTER(c_float)]
+predict.restype = POINTER(c_float)
+
+set_gpu = lib.cuda_set_device
+set_gpu.argtypes = [c_int]
+
+make_image = lib.make_image
+make_image.argtypes = [c_int, c_int, c_int]
+make_image.restype = IMAGE
+
+make_boxes = lib.make_boxes
+make_boxes.argtypes = [c_void_p]
+make_boxes.restype = POINTER(BOX)
+
+free_ptrs = lib.free_ptrs
+free_ptrs.argtypes = [POINTER(c_void_p), c_int]
+
+num_boxes = lib.num_boxes
+num_boxes.argtypes = [c_void_p]
+num_boxes.restype = c_int
+
+make_probs = lib.make_probs
+make_probs.argtypes = [c_void_p]
+make_probs.restype = POINTER(POINTER(c_float))
+
+detect = lib.network_predict
+detect.argtypes = [c_void_p, IMAGE, c_float, c_float, c_float, POINTER(BOX), POINTER(POINTER(c_float))]
+
+reset_rnn = lib.reset_rnn
+reset_rnn.argtypes = [c_void_p]
+
+load_net = lib.load_network
+load_net.argtypes = [c_char_p, c_char_p, c_int]
+load_net.restype = c_void_p
+
+free_image = lib.free_image
+free_image.argtypes = [IMAGE]
+
+letterbox_image = lib.letterbox_image
+letterbox_image.argtypes = [IMAGE, c_int, c_int]
+letterbox_image.restype = IMAGE
+
+load_meta = lib.get_metadata
+lib.get_metadata.argtypes = [c_char_p]
+lib.get_metadata.restype = METADATA
+
+load_image = lib.load_image_color
+load_image.argtypes = [c_char_p, c_int, c_int]
+load_image.restype = IMAGE
+
+rgbgr_image = lib.rgbgr_image
+rgbgr_image.argtypes = [IMAGE]
+
+predict_image = lib.network_predict_image
+predict_image.argtypes = [c_void_p, IMAGE]
+predict_image.restype = POINTER(c_float)
+
+network_detect = lib.network_detect
+network_detect.argtypes = [c_void_p, IMAGE, c_float, c_float, c_float, POINTER(BOX), POINTER(POINTER(c_float))]
+
+test_detector = lib.test_detector
+test_detector.argtypes = [c_char_p, c_void_p, c_char_p, c_float, c_float, c_char_p, c_int]
+
+# void test_detector(char *datacfg, char *cfgfile, char *weightfile, char *filename, float thresh, float hier_thresh, char *outfile, int fullscreen)
+
+net = load_net("darknet/files/yolo.cfg", "darknet/files/yolo.weights", 0)
+# meta = load_meta("/home/e0358-3/asst-2/darknet/cfg/coco.data")
+
+# void test_detector(char *datacfg, void *net_ptr, char *filename, float thresh, float hier_thresh, char *outfile, int fullscreen)
+
+def classify(net, meta, im):
+    out = predict_image(net, im)
+    res = []
+    for i in range(meta.classes):
+        res.append((meta.names[i], out[i]))
+    res = sorted(res, key=lambda x: -x[1])
+    return res
+
+def detect(net, meta, image, thresh=.5, hier_thresh=.5, nms=.45):
+    im = load_image(image, 0, 0)
+    boxes = make_boxes(net)
+    probs = make_probs(net)
+    num =   num_boxes(net)
+    network_detect(net, im, thresh, hier_thresh, nms, boxes, probs)
+    res = []
+    for j in range(num):
+        for i in range(meta.classes):
+            if probs[j][i] > 0:
+                res.append((meta.names[i], probs[j][i], (boxes[j].x, boxes[j].y, boxes[j].w, boxes[j].h)))
+    res = sorted(res, key=lambda x: -x[1])
+
+    free_image(im)
+    free_ptrs(cast(probs, POINTER(c_void_p)), num)
+    return res
+
 def darknet_polymage(frame, lib_func):
     rows = frame.shape[0]
     cols = frame.shape[1]
     total_pad = 56
-    res = np.empty((rows, cols), np.float32)
-    lib_func(ctypes.c_int(cols+total_pad), \
-             ctypes.c_int(rows+total_pad), \
-             ctypes.c_void_p(frame.ctypes.data), \
-             ctypes.c_void_p(res.ctypes.data))
-    return res
+    imwrite('messigray.png', frame)
+    # IMAGE im;
+    # im["w"] = frame[0]
+    # im[""] = frame[1]
+    # im->c = 3
+    # im->data = ctypes.c_char_p()
+    
+    # res = detect(net, meta, 'messigray.png')
+    test_detector("darknet/files/coco.data", net, '/home/e0358-3/asst-2/polymage/sandbox/video_demo/messigray.png', .5, .5, "try",0)
+    # lib_func(ctypes.c_int(cols), \
+    #          ctypes.c_int(rows), \
+    #          net, \
+    #          ctypes.c_char_p("/home/e0358-3/asst-2/darknet/cfg/coco.data"))
+    # ret = np.empty((frame.shape[0], frame.shape[1], 3), np.uint8)
+    ret = imread('try.jpg', IMREAD_COLOR)
+    return ret
 
 def add_darknet_app(app_id):
+
     # 1. modes
     modes = [ModeType.P_OPT]
     # 2. modes that need shared library
